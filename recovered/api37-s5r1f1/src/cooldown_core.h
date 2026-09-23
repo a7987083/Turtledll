3f49f32250668d9a8b9d17ec3797a499716d3a3b
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-24 04:29:36 +0800

    recovery: add Cooldown world-leave lifecycle entrypoint

--- recovered/api37-s5r1f1/src/cooldown_core.h
+++ recovered/api37-s5r1f1/src/cooldown_core.h
@@ -1,9 +1,4 @@
 #pragma once
 #include "lua50.h"
-namespace TysCooldownCore {
-bool initialize();
-const char* status();
-int dispatchStatus(Lua50::State L);
-int dispatchGet(Lua50::State L);
-int dispatchList(Lua50::State L);
-}
+namespace TysCooldownCore { bool initialize(); const char* status();
+void onWorldLeaving(); int dispatchStatus(Lua50::State L); int dispatchGet(Lua50::State L); int dispatchList(Lua50::State L); }
